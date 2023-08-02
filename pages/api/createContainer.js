f083ec48bbec1eace3f0e5e7be7b94f38e918c64
Akshayksalian <akshayksalian@gmail.com>
2023-08-02 23:10:59 +0530

    Start call is changed to server side call

--- pages/api/createContainer.js
+++ pages/api/createContainer.js
@@ -60,7 +60,7 @@ export default async function handler(req, res) {
       publishParameter
     );
 
-    res.status(200).json(publishedMediaId.id);
+    res.status(200).json(publishedMediaId.data);
   } catch (error) {
     res.status(500).json(error);
   }
--- pages/api/test.js
+++ pages/api/test.js
@@ -1,36 +1,49 @@
 import axios from "axios";
-import db from "@/firebase/initFirebase";
-import {
-  doc,
-  onSnapshot,
-  updateDoc,
-  setDoc,
-  deleteDoc,
-  collection,
-  serverTimestamp,
-  getDocs,
-  query,
-  where,
-  orderBy,
-  limit,
-} from "firebase/firestore";
 
 export default async function handler(req, res) {
   try {
-    const params = {
-      collectionId: "NewsData",
-      documentId: "valuesForValidation",
-      data: {
-        lastPublishedDate: "2023-07-20T16:31:00A",
-      },
-    };
-
-    const updatePublishDateToDb = await axios.post(
-      "http://localhost:3000/api/firebase/edit",
-      params
+    const newsData = await axios.get(
+      `http://${req.headers.host}/api/getNewsData`
     );
 
-    res.status(200).json(req.headers.host);
+    const previousPublishedData = await axios.get(
+      `http://${req.headers.host}/api/firebase/get`
+    );
+
+    if (
+      newsData.data.articles[0].publishedAt !=
+      previousPublishedData.data.lastPublishedDate
+    ) {
+      const editParams = {
+        collectionId: "NewsData",
+        documentId: "valuesForValidation",
+        data: {
+          lastPublishedDate: newsData.data.articles[0].publishedAt,
+        },
+      };
+
+      const updateDate = await axios.post(
+        `http://${req.headers.host}/api/firebase/edit`,
+        editParams
+      );
+
+      const dataForContainer = {
+        title: newsData.data.articles[0].title,
+        description: newsData.data.articles[0].description,
+        content: newsData.data.articles[0].content,
+        imageUrl: newsData.data.articles[0].image,
+        publishedAt: newsData.data.articles[0].publishedAt,
+      };
+
+      const postData = await axios.post(
+        `http://${req.headers.host}/api/createContainer`,
+        dataForContainer
+      );
+
+      res.status(200).json(postData.data);
+    } else {
+      res.status(404).json({ error: "No data was published" });
+    }
   } catch (error) {
     res.status(500).json(error);
   }
